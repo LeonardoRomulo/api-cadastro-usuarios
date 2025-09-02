ea41cfd66998bcab045aabe8b3525f6a2951de2d
LeonardoRomulo <leo.romulo15@gmail.com>
2025-09-02 12:00:57 -0300

    feat(model): adicionado a classe Usuario com nome, email e telefone

---
+++ model/usuario.js
@@ -0,0 +1,9 @@
+class usuario {
+    constructor(nome, email, telefone){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
+
+export default usuario;
